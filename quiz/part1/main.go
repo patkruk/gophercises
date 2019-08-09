a0d111772b665253cd1dc5c8ff4fe861c54e786f
Patryk Kruk <patkruk@gmail.com>
2019-08-08 23:14:20 -0400

    Finished quiz part 1

--- quiz/part1/main.go
+++ quiz/part1/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"flag"
-	"io/ioutil"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 )
+
+// CsvLine represents each line in the csv file
+type CsvLine struct {
+	Question string
+	Answer   string
+}
+
+// Results has the correct and wrong counts
+type Results struct {
+	CorrectCount int
+	WrongCount   int
+}
 
 func check(err error) {
 	if err != nil {
@@ -14,22 +30,42 @@ func check(err error) {
 
 func main() {
 	// see if the filename has been provided
-	csv := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	fileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 
 	// once all flags are declared, call flag.Parse() to execute the command-line parsing
 	flag.Parse()
 
-	// read the file
-	file, err := ioutil.ReadFile(*csv)
+	file, err := os.Open(*fileName)
 	check(err)
 
-	// read individual lines
+	defer file.Close()
 
-	// parse csv
+	// read the file and parse it (csv)
+	lines, err := csv.NewReader(file).ReadAll()
+	check(err)
 
-	// ask the question
+	results := Results{}
 
-	// store the answer
+	for _, line := range lines {
+		data := CsvLine{
+			Question: line[0],
+			Answer:   line[1],
+		}
 
-	// keep track of right & wrong answers
+		// ask the question
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("%v = ?\n", data.Question)
+
+		// check the answer (keep track of right & wrong answers)
+		answer, _ := reader.ReadString('\n')
+		answer = strings.TrimSuffix(answer, "\n")
+
+		if answer == data.Answer {
+			results.CorrectCount++
+		} else {
+			results.WrongCount++
+		}
+	}
+
+	fmt.Printf("\nCorrect answers count: %d. Wrong answers count: %d.", results.CorrectCount, results.WrongCount)
 }
