b38f3476d2628b3c74180351fbb36640378b3232
Patryk Kruk <patkruk@gmail.com>
2019-08-14 11:25:02 -0400

    Adding support for yaml files to UrlShortener

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/patkruk/gophercises/urlshort"
 )
+
+var yaml = `
+- path: /urlshort
+  url: http://www.google.com
+- path: /urlshort-final
+  url: http://www.wp.pl
+`
 
 func main() {
 	mux := defaultMux()
@@ -17,15 +28,28 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yml := []byte(yaml)
+
+	fileName := flag.String("yml", "", "a yml file'")
+	flag.Parse()
+
+	// if a yaml file specified, use it
+	if isFlagPassed("yml") {
+		file, err := os.Open(*fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer file.Close()
+
+		yml, err = ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Build the YAMLHandler using the mapHandler as the fallback
+	yamlHandler, err := urlshort.YAMLHandler(yml, mapHandler)
 	if err != nil {
 		panic(err)
 	}
@@ -36,9 +60,22 @@ func main() {
 func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
+
 	return mux
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
+}
+
+func isFlagPassed(name string) bool {
+	found := false
+
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+
+	return found
 }
