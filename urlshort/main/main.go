1140d27863bad654ba4b042272cc15eb97432077
Patryk Kruk <patkruk@gmail.com>
2019-08-15 23:22:02 -0400

    Adding Bbolt database to UrlShortener

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/patkruk/gophercises/urlshort"
+	bolt "go.etcd.io/bbolt"
 )
 
 var yaml = `
@@ -19,17 +20,17 @@ var yaml = `
 `
 
 func main() {
+	// default fallback
 	mux := defaultMux()
 
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	// // Build the MapHandler using the mux as the fallback
+	pathsToUrls, err := getMapOfPathUrls()
+	if err != nil {
+		log.Fatal(err)
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	yml := []byte(yaml)
-
 	fileName := flag.String("yml", "", "a yml file'")
 	flag.Parse()
 
@@ -93,4 +94,73 @@ func isFlagPassed(name string) bool {
 	})
 
 	return found
+}
+
+func getMapOfPathUrls() (map[string]string, error) {
+	// to be returned
+	result := make(map[string]string)
+
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("my_bbolt.db", 0600, nil)
+	if err != nil {
+		return result, err
+	}
+	defer db.Close()
+
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	bucketName := "urls"
+
+	// store some data in a bucket (only if it does not exist yet)
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+		if err != nil {
+			return err
+		}
+
+		for key, val := range pathsToUrls {
+			value := bucket.Get([]byte(key))
+			// only add if it does not exist yet
+			if value == nil {
+				fmt.Println("Adding new data to the bucket")
+
+				err = bucket.Put([]byte(key), []byte(val))
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return result, err
+	}
+
+	// retrieve the data
+	err = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bucketName))
+		if bucket == nil {
+			return fmt.Errorf("Bucket %s not found", bucketName)
+		}
+
+		for key := range pathsToUrls {
+			value := bucket.Get([]byte(key))
+			if value == nil {
+				return fmt.Errorf("Unable to fetch data for %s", key)
+			}
+
+			result[key] = string(value)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
 }
