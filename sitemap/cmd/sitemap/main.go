4e4a6736adb1443e104c31e8e06da9bfa75ecf78
Patryk Kruk <patkruk@gmail.com>
2019-08-26 22:32:40 -0400

    Working on the sitemap builder

--- sitemap/cmd/sitemap/main.go
+++ sitemap/cmd/sitemap/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
+
+	"github.com/patkruk/gophercises/linkparser"
 )
 
 // Links contains a map of all links found on a site
@@ -18,28 +20,59 @@ type Link struct {
 }
 
 func main() {
-	url := flag.String("url", "http://agajewelrydesigns.com", "a domain of the website we are creating a sitemap for")
+	links := make(map[string]Link)
+
+	domain := flag.String("domain", "http://agajewelrydesigns.com", "a domain of the website we are creating a sitemap for")
 	flag.Parse()
 
-	response, err := makeGetCall(*url)
+	// call the specified domain
+	response, err := makeGetCall(*domain)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(response))
+	// parse all links on the site
+	urls := parseLinks(response)
+
+	// add new links to the map of links
+	for _, link := range urls {
+		// skip links to the homepage
+		if link.Href == "/" || link.Href == *domain || link.Href == "" {
+			continue
+		}
+
+		// skip mailto links
+
+		// prepend domain if missing
+		href := link.Href
+		if string(href[0]) == "/" {
+			href = *domain + href
+		}
+
+		// skip links to external sites
+
+		// check if link already exists before setting
+		// we don't want to overwrite
+
+		links[href] = Link{
+			URL:     href,
+			Visited: false,
+		}
+	}
+
+	fmt.Println(links)
 }
 
-func makeGetCall(url string) ([]byte, error) {
+func parseLinks(r io.Reader) []linkparser.Link {
+	return linkparser.Parse(r)
+}
+
+func makeGetCall(url string) (io.Reader, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	// defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return body, nil
+	return resp.Body, nil
 }
