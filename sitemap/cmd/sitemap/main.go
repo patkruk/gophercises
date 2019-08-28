39630dae22d49dea21f17f42963c2d38dd7926fc
Patryk Kruk <patkruk@gmail.com>
2019-08-27 22:36:08 -0400

    Adding more href checks to the sitemap builder

--- sitemap/cmd/sitemap/main.go
+++ sitemap/cmd/sitemap/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	urlPackage "net/url"
 
 	"github.com/patkruk/gophercises/linkparser"
 )
@@ -22,8 +23,12 @@ type Link struct {
 func main() {
 	links := make(map[string]Link)
 
-	domain := flag.String("domain", "http://agajewelrydesigns.com", "a domain of the website we are creating a sitemap for")
+	domain := flag.String("domain", "http://agajewelrydesigns.com", "a full domain of the website we are creating a sitemap for")
 	flag.Parse()
+
+	if !isValidURL(*domain) {
+		log.Fatal("Invalid domain provided")
+	}
 
 	// call the specified domain
 	response, err := makeGetCall(*domain)
@@ -36,30 +41,55 @@ func main() {
 
 	// add new links to the map of links
 	for _, link := range urls {
+		if link.Href == "" {
+			continue
+		}
+
+		href := link.Href
+
 		// skip links to the homepage
-		if link.Href == "/" || link.Href == *domain || link.Href == "" {
+		if href == "/" || href == *domain {
 			continue
 		}
 
 		// skip mailto links
+		if len(href) >= 6 && href[0:6] == "mailto" {
+			continue
+		}
 
 		// prepend domain if missing
-		href := link.Href
 		if string(href[0]) == "/" {
 			href = *domain + href
 		}
 
+		// skip invalid urls
+		if !isValidURL(href) {
+			continue
+		}
+
 		// skip links to external sites
+		strippedDomain, err := stripProtocolFromURL(*domain)
+		if err != nil {
+			log.Fatal(err)
+		}
+		strippedHref, err := stripProtocolFromURL(href)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(strippedDomain) > len(strippedHref) || strippedHref[0:len(strippedDomain)] != strippedDomain {
+			continue
+		}
 
-		// check if link already exists before setting
-		// we don't want to overwrite
-
-		links[href] = Link{
-			URL:     href,
-			Visited: false,
+		// store the link only if it does not exist
+		if _, ok := links[href]; !ok {
+			links[href] = Link{
+				URL:     href,
+				Visited: false,
+			}
 		}
 	}
 
+	fmt.Println(len(links))
 	fmt.Println(links)
 }
 
@@ -75,4 +105,41 @@ func makeGetCall(url string) (io.Reader, error) {
 	// defer resp.Body.Close()
 
 	return resp.Body, nil
+}
+
+// stripProtocolFromURL returns a "naked" URL without the protocol information
+// (e.g. http://www.example.com => example.com)
+func stripProtocolFromURL(URL string) (string, error) {
+	if len(URL) < 4 {
+		return "", fmt.Errorf("Provided URL is less than required 4 characters")
+	}
+
+	// remove "http" & "https"
+	if len(URL) >= 5 && URL[0:5] == "https" {
+		URL = URL[5:]
+	} else if URL[0:4] == "http" {
+		URL = URL[4:]
+	}
+
+	// remove "://"
+	if len(URL) >= 3 && URL[0:3] == "://" {
+		URL = URL[3:]
+	}
+
+	// remove "www."
+	if len(URL) >= 4 && URL[0:4] == "www." {
+		URL = URL[4:]
+	}
+
+	return URL, nil
+}
+
+// isValidURL indicates if a given url is valid
+func isValidURL(url string) bool {
+	_, err := urlPackage.ParseRequestURI(url)
+	if err != nil {
+		return false
+	}
+
+	return true
 }
