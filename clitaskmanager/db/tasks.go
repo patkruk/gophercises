3223bd9cb6e434c2b4fe5d18a4c5082a3af0d919
Patryk Kruk <patkruk@gmail.com>
2019-10-06 22:07:28 -0400

    More cli task manager changes

--- clitaskmanager/db/tasks.go
+++ clitaskmanager/db/tasks.go
@@ -50,7 +50,36 @@ func CreateTask(task string) (int, error) {
 		return -1, err
 	}
 
-	return 0, nil
+	return id, nil
+}
+
+func AllTasks() ([]Task, error) {
+	var tasks []Task
+
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func DeleteTask(key int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		return b.Delete(itob(key))
+	})
 }
 
 func itob(v int) []byte {
